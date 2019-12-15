6e01be9fe1436124e7d94fad809a9d3b3cb279a4
Mr. Appu™ <46938448+appuchias@users.noreply.github.com>
2019-12-15 03:06:35 +0100

    Graphic improvement

--- COG_rrss.py
+++ COG_rrss.py
@@ -9,32 +9,37 @@ class InstaCog(commands.Cog):
         self.ig = Instagram()
 
     @commands.command()
-    async def insta(self, ctx, user: str = None):
-        if user == None:
-            await ctx.send("Especifica usuario que buscar")
+    async def insta(self, ctx, perfil: str = None):
         try:
-            all_medias = self.ig.get_medias(user, 10)
+            all_medias = self.ig.get_medias(perfil, 10)
         except Exception as error:
             await ctx.send("Error: ", error)
             return
-        last_pic = all_medias[0]
-        if last_pic.type == "image":
-            embed = discord.Embed(title=f"Última imagen de [{user}] en Instagram", description="By Mr. Appu™", url=last_pic.image_high_resolution_url, color=0xf03c4d)
-            embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-            embed.set_image(url=last_pic.image_high_resolution_url)
-            embed.add_field(name="Pie de foto:", value=last_pic.caption, inline=True)
-            embed.add_field(name="Likes:", value=last_pic.likes_count, inline=True)
-            embed.add_field(name="Comentarios:", value=last_pic.comments_count, inline=True)
-            await ctx.send(embed=embed)
+        for img in all_medias:
+            if img.type == "image":
+                embed = discord.Embed(title=f"Última imagen de [{perfil}] en Instagram", description="By Mr. Appu™", url=img.image_high_resolution_url, color=0xf03c4d)
+                embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+                embed.set_image(url=img.image_high_resolution_url)
+                embed.add_field(name="Pie de foto:", value=img.caption, inline=True)
+                embed.add_field(name="Likes:", value=img.likes_count, inline=True)
+                embed.add_field(name="Comentarios:", value=img.comments_count, inline=True)
+                await ctx.send(embed=embed)
+                return
 
     @commands.command()
     async def twitter(self, ctx, user: str = None):
         if user == None:
             await ctx.send("Especifica usuario que buscar")
-        embed = discord.Embed(title="Twitter info", description="By Appu")
+
+        embed = discord.Embed(title=f"Últimos 10 tweets de {user}", description="By Mr. Appu™", color=0x1da1f3, url=f"https://twitter.com/{user}")
+
+        cnt = 1
         for tweet in get_tweets(user, pages=1):
-            embed.add_field(name=f"Tweet", value=f"{(tweet['text']).replace('pic', 'https://pic')}\n{tweet['likes']} likes <3!\nTweet: https://twitter.com/{user}/status/{tweet['tweetId']}", inline=False)
-        await ctx.send(embed=embed)
+            embed.add_field(name=f"**Tweet** ***{cnt}*** **de 10**", value=f"```{(tweet['text']).replace('pic', 'https://pic')}```:heart: {tweet['likes']}      -      :repeat: {tweet['retweets']}\n(https://twitter.com/{user}/status/{tweet['tweetId']})", inline=False)
+            if cnt >= 10:
+                await ctx.send(embed=embed)
+                return
+            cnt += 1
 
 def setup(client):
     client.add_cog(InstaCog(client))
